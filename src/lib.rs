2864ec29519f222e8cf982b1b8d2130a509ffa51
ssword <ssword@gmail.com>
2022-10-07 23:52:02 +0800

    Initial commit for template engine.

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+// Each line in input can be of one of following types
+#[derive(PartialEq, Debug)]
+pub enum ContentType {
+    Literal(String),
+    TemplateVariable(ExpressionData),
+    Tag(TagType),
+    Unrecognized,
+}
+
+#[derive(PartialEq, Debug)]
+pub enum TagType {
+    ForTag,
+    IfTag,
+}
+
+#[derive(PartialEq, Debug)]
+pub struct ExpressionData {
+    pub head: Option<String>,
+    pub variable: String,
+    pub tail: Option<String>,
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_literal_test() {
+        let s = "<h1>Hello world</h1>";
+        assert_eq!(ContentType::Literal(s.to_string()), get_content_type(s));
+    }
+}
