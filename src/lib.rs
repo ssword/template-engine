589d831bc247e4f8aa8d6ff1afc26c5114cb1823
ssword <ssword@gmail.com>
2022-10-09 23:48:04 +0800

    added get_content_type function

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,32 @@ pub struct ExpressionData {
     pub tail: Option<String>,
 }
 
+pub fn get_content_type(input_line: &str) -> ContentType {
+    let is_tag_expression = check_matching_pair(&input_line, "{%", "%}");
+    let is_for_tag = (check_symbol_string(&input_line, "for")
+        && check_symbol_string(&input_line, "in"))
+        || check_symbol_string(&input_line, "endfor");
+    let is_if_tag = check_symbol_string(&input_line, "if") ||
+        check_symbol_string(&input_line, "endif");
+
+    let is_template_variable = check_matching_pair(&input_line, "{{", "}}");
+    let return_val;
+
+    if is_tag_expression && is_for_tag {
+        return_val = ContentType::Tag(TagType::ForTag);
+    } else if is_tag_expression && is_if_tag {
+        return_val = ContentType::Tag(TagType::IfTag);
+    } else if is_template_variable {
+        let content = get_expression_data(&input_line);
+        return_val = ContentType::TemplateVariable(content);
+    } else if !is_tag_expression && !is_template_variable {
+        return_val = ContentType::Literal(input_line.to_string());
+    } else {
+        return_val = ContentType::Unrecognized;
+    }
+    return_val
+}
+
 
 
 #[cfg(test)]
@@ -61,5 +87,4 @@ mod tests {
             get_content_type("{% if name == 'bob' %}")
         );
     }
-
 }
